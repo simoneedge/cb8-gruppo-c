c90ee977b4babadcf698ad24f10f05641fa92720
Giovanni Scivoli <scivoligiovanni@gmail.com>
2024-04-04 17:04:18 +0200

    added bonestructure to the BE for sports

---
+++ src/models/Sport.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const sportSchema = new mongoose.Schema({
+  sportName: {
+    type: String,
+    required: true,
+  },
+  playerCount: {
+    type: Number,
+    required: true,
+  },
+  roles: {
+    type: Array,
+    required: false,
+  },
+});
+
+module.exports = mongoose.models.Sport || mongoose.model("Sport", sportSchema);
