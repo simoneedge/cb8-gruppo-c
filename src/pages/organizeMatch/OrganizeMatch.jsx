7b6994a7efdbc1242486be6e6c03577cef714a31
ChiaraJF <chiara.fell87@gmail.com>
2024-04-07 18:19:03 +0200

    style

--- src/pages/organizeMatch/OrganizeMatch.jsx
+++ src/pages/organizeMatch/OrganizeMatch.jsx
@@ -1,9 +1,11 @@
 import styles from "./index.module.scss";
 import Stadium from "@/components/stadium";
+import Button from "@/components/button";
 
 export default function OrganizeMatch() {
   return (
     <div className={styles.container}>
+      <Button text="Partecipa ad un match" className={styles.orgButton} />
       <h1 className={styles.title}>Organizza Match</h1>
       <Stadium />
     </div>
