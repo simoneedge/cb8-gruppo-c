168377651220d3ce9b9a0e9e0bc88325fb044819
Chiara JF <51326610+chiarajf@users.noreply.github.com>
2024-04-07 20:05:28 +0200

    Update SignIn.jsx

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -1,10 +1,8 @@
-import LoginForm from "@/components/loginForm";
 import styles from "./index.module.scss";
 
 export default function SignIn({ auth }) {
   return (
     <div className={styles.container}>
-      <LoginForm setIsAuthenticated={auth} />
     </div>
   );
 }
