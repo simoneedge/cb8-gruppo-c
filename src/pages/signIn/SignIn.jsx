e82ae497bb5333d70aca28a574bffd6791a53136
ChiaraJF <chiara.fell87@gmail.com>
2024-04-09 00:17:21 +0200

    Completamento pagina login e homepage.

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -8,33 +8,47 @@ export default function SignIn({ username }) {
   const router = useRouter();
   const { msg } = router.query;
   return (
-    <div className={styles.container}>
-      <Image src="/JustPlay.svg" alt="logo" width={300} height={300} />
+    <div className={styles.wrapper}>
+      <Image
+        src="/JustPlay.svg"
+        alt="logo"
+        width={300}
+        height={300}
+        className={styles.logo}
+      />
 
       {msg ? <h3 className="red">{msg}</h3> : <></>}
-      <h4 className={styles.title}>Log in</h4>
-      <form action="/api/login" method="POST" className={styles.form}>
-        <input
-          minLength="3"
-          name="username"
-          id="username"
-          type="text"
-          placeholder="username"
-          required
-        ></input>
-        <input
-          minLength="5"
-          name="password"
-          id="password"
-          type="password"
-          placeholder="password"
-          required
-        ></input>
-        <input type="submit" value="Sign In" className={styles.button} />
-      </form>
-      <Link href="/signUp" className={styles.link}>
-        or SignUp
-      </Link>
+      <div className={styles.container}>
+        <h4 className={styles.title}>
+          {" "}
+          Ciao,<br></br> accedi per continuare
+        </h4>
+        <form action="/api/login" method="POST" className={styles.form}>
+          <input
+            minLength="3"
+            name="username"
+            id="username"
+            type="text"
+            placeholder="username"
+            required
+          ></input>
+          <input
+            minLength="5"
+            name="password"
+            id="password"
+            type="password"
+            placeholder="password"
+            required
+          ></input>
+          <input type="submit" value="Sign In" className={styles.button} />
+        </form>
+        <p>
+          Non hai ancora un account?{" "}
+          <Link href="/signUp" className={styles.link}>
+            Iscriviti qui
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
