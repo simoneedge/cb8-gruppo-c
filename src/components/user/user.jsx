1c67d564a68ed2ccb9236b670bc9d08a7c2f71a8
PinaLiu <119705587+PinaLiu@users.noreply.github.com>
2024-04-08 17:36:46 +0200

    Componente user e pagina TeamFormations

---
+++ src/components/user/user.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Avatar from "boring-avatars";
+import { faker } from "@faker-js/faker";
+import styles from "./index.module.scss";
+
+const User = () => {
+  const name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+
+  return (
+    <div className={styles.user}>
+      <Avatar
+        size={40}
+        name={name}
+        variant="beam"
+        square={true} // false la rende tonda
+        colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+        className={styles.avatar}
+      />
+      <div className={styles.details}>
+        <h2 className={styles.name}>{name}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default User;
