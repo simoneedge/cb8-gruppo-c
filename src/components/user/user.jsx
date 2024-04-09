d973c002d17fb9b138dfe9a8372094ac43975f84
ChiaraJF <chiara.fell87@gmail.com>
2024-04-09 14:10:48 +0200

    editing style pagina formazioni

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import Avatar from "boring-avatars";
 import { faker } from "@faker-js/faker";
 import styles from "./index.module.scss";
+import Avatar from "boring-avatars";
 
 const User = () => {
   const name = `${faker.person.firstName()} ${faker.person.lastName()}`;
 
   return (
     <div className={styles.user}>
-      <Avatar
-        size={40}
-        name={name}
-        variant="beam"
-        square={true} // false la rende tonda
-        colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
-        className={styles.avatar}
-      />
+      <div className={styles.container_image}>
+        <Avatar
+          size={50}
+          name={name}
+          variant="beam"
+          square={true} // false la rende tonda
+          colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+        />
+      </div>
       <div className={styles.details}>
         <h2 className={styles.name}>{name}</h2>
       </div>
