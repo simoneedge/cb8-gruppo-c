4b6c56aecf9a4e333200da2f10aaf8666ce53f02
Martina Forcieri <maey988@gmail.com>
2024-04-09 12:25:14 +0200

    modal/currentGame work in progress

--- src/pages/organizeMatch/OrganizeMatch.jsx
+++ src/pages/organizeMatch/OrganizeMatch.jsx
@@ -6,9 +6,22 @@ export default function OrganizeMatch() {
   return (
     <>
       <div className={styles.container}>
+<<<<<<< HEAD
         <h1 className={styles.title}>Organizza Match</h1>
         <Stadium />
         <Modal />
+=======
+        <h1 className={styles.title}>Organizza la tua partita</h1>
+        <p>
+          Inserisci la città in cui vuoi giocare e seleziona la struttura
+          sportiva che hai prenotato
+        </p>
+        <Stadium />
+      </div>
+      <div className={styles.fixed}>
+        <h4>oppure</h4>
+        <Button text="Partecipa ad un match" className={styles.orgButton} />
+>>>>>>> a64140659a1e18f3e6fea635205c171709e7b7f5
       </div>
     </>
   );
