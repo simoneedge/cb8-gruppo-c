ef617b0edcbbe04944d770bdd29134b0c07932a3
viciorog <vinciorogato@gmail.com>
2024-04-10 14:53:11 +0200

    creata pagina details

--- src/pages/matchDetails/MatchDetails.jsx
+++ src/pages/matchDetails/MatchDetails.jsx
@@ -1,9 +1,42 @@
 import styles from "./index.module.scss";
+import Image from "next/image";
+import BlueShield from "../../../public/Blue-shield.svg";
+import RedShield from "../../../public/Red-shield.svg";
+import Versus from "../../../public/Versus.svg";
+import Button from "@/components/button";
+import { useState, useEffect } from "react";
 
 export default function MatchDetails() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Dettagli partite</h1>
-    </div>
+    <>
+      <div className={styles.teamFormations}>
+        <div className={styles.team}>
+          <Image src={BlueShield} alt="Team Blue" width={295} height={234} />
+        </div>
+        <div className={styles.vs}>
+          <Image
+            src={Versus}
+            alt="Versus"
+            width={195}
+            height={134}
+            className={styles.imgVs}
+          />
+        </div>
+        <div className={styles.team}>
+          <Image src={RedShield} alt="Team Red" width={295} height={234} />
+        </div>
+      </div>
+      <form action="" className={styles.form}>
+        <input
+          type="text"
+          placeholder="Sport scelto"
+          className={styles.sport}
+        />
+        <input type="text" placeholder="data" />
+        <input type="text" placeholder="Orario" />
+        <input type="text" placeholder="Costo Opzionale" />
+        <input type="submit" value="Partecipa ora" className={styles.btn} />
+      </form>
+    </>
   );
 }
