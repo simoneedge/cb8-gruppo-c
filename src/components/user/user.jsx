e969073e1bbe184804997711f43dc11600237790
PinaLiu <119705587+PinaLiu@users.noreply.github.com>
2024-04-12 11:17:12 +0200

    modifica componente user

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
+
 import styles from "./index.module.scss";
 import Avatar from "boring-avatars";
 
-const User = () => {
-  const name = `${faker.person.firstName()} ${faker.person.lastName()}`;
+const User = ({ name, surname }) => {
+  const fullName = `${name} ${surname}`;
 
   return (
     <div className={styles.user}>
       <div className={styles.container_image}>
         <Avatar
           size={50}
-          name={name}
+          name={fullName}
           variant="beam"
           square={true} // false la rende tonda
           colors={["#216869", "#57b288", "#c4e6c9", "#e9e9e9"]}
         />
       </div>
       <div className={styles.details}>
-        <h2 className={styles.name}>{name}</h2>
+        <h2 className={styles.name}>{fullName}</h2>
       </div>
     </div>
   );
