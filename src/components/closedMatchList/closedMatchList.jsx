efac7a4cc600d6bb86f07f4d235b4a85900e6aec
viciorog <vinciorogato@gmail.com>
2024-04-13 21:17:39 +0200

    creata pagina cronologia partite

---
+++ src/components/closedMatchList/closedMatchList.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+import CardMatch from "../cardMatch";
+import { getCookie } from "cookies-next";
+
+export default function ClosedMatchList() {
+  const [matches, setMatches] = useState([]);
+  const user = getCookie("userData");
+
+  useEffect(() => {
+    fetch("/api/matches")
+      .then((res) => res.json())
+      .then((data) => {
+        const userMatches = data.data.filter((match) => {
+          return (
+            (match.team1.includes(user) || match.team2.includes(user)) &&
+            match.inProgress === false
+          );
+        });
+        setMatches(userMatches);
+      })
+      .catch((error) => {
+        console.error("Errore durante il recupero dei match:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      {matches.map((match) => (
+        <CardMatch
+          key={match.id}
+          location={match.location}
+          sport={match.sport}
+          _id={match._id}
+        />
+      ))}
+    </>
+  );
+}
