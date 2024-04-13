46ee3c97ac1abfe3b2107eb0015dacb6a7ea508a
viciorog <vinciorogato@gmail.com>
2024-04-13 15:50:46 +0200

    fixata fetch match details

--- src/components/matchList/MatchList.jsx
+++ src/components/matchList/MatchList.jsx
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
 
 import CardMatch from "../cardMatch";
+import { getCookie } from "cookies-next";
 
 export default function MatchList() {
   const [matches, setMatches] = useState([]);
+  const selectedSport = getCookie("selectedSport");
 
   useEffect(() => {
     fetch("/api/matches")
       .then((res) => res.json())
       .then((data) => {
-        setMatches(data.data);
+        const sportMatches = data.data.filter((match) => {
+          return match.sport.includes(selectedSport);
+        });
+        setMatches(sportMatches);
+      })
+      .catch((error) => {
+        console.error("Errore durante il recupero dei match:", error);
       });
   }, []);
 
