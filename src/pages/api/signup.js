536d87e40681ff9073221acf294ad56d4367c3b8
Giovanni Scivoli <scivoligiovanni@gmail.com>
2024-04-15 22:40:06 +0200

    bug signup

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -34,6 +34,7 @@ export default async function handler(req, res) {
         location: location,
         sports: sports,
         email: email,
+        birthdate: Date.now(),
       });
 
       await collection.insertOne(newUser);
